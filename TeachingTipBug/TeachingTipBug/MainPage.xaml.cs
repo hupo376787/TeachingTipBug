319f4d7afcce954e2d9959689f1c169b568433de
Vincent Wang <376787823@qq.com>
2019-06-14 10:29:46 +0800

    Add files via upload

---
+++ TeachingTipBug/TeachingTipBug/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using MUXC = Microsoft.UI.Xaml.Controls;
+
+// https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x804 上介绍了“空白页”项模板
+
+namespace TeachingTipBug
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Button_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            MUXC.TeachingTip tip = new MUXC.TeachingTip();
+            tip.Title = "Error";
+            tip.Subtitle = "It seems you are not connected to the Internet :(";
+            tip.CloseButtonContent = "OK";
+            tip.IsLightDismissEnabled = true;
+            tip.IsOpen = true;
+        }
+    }
+}
